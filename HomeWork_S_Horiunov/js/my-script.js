18a1941393b9300cb70ec51bcd20a2545fce5744
Serhii Horiunov <serghory@gmail.com>
2023-04-10 17:53:06 +0200

    Create my-script.js

---
+++ HomeWork_S_Horiunov/js/my-script.js
@@ -0,0 +1,75 @@
+//***1***
+// Робота зі змінними
+//Оголосіть дві змінні: name та city.
+//let name;
+//let city;
+//Присвойте значення "Іван" змінній name.
+ //   name = "Іван";
+//Скопіюйте значення зі змінної name в city.
+ //   city = name;
+//Виведіть значення змінної city, використовуючи функцію console.log(яка повинна показати “Іван”).
+ //    console.log(city);
+
+//***2***
+//Який буде результат виконання скрипта?
+//let name = "Olga";
+//console.log(`привіт ${1}`); // привіт 1
+//console.log(`привіт ${"name"}`); // привіт name
+//console.log(`привіт ${name}`); // привіт Olga
+
+//***3***
+//Видобути число зі змінних
+//let a = "5";
+//let b = "13cvb";
+//let c = "12.9sxdcfgv";
+// вивести в консоль тип
+//console.log(typeof Number.parseInt(a));
+//console.log(typeof Number.parseInt(b));
+//console.log(typeof Number.parseFloat(c));
+
+//***4***
+//Зробіть, щоб 0.1 + 0.2 = 0.3
+//console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+//***5**
+//Поверніть найбільше число с набору 20, 10, 50, 40
+//console.log(Math.max(20, 10, 50, 40));
+
+//***6**
+//Поверніть випадкове число в діапазоні від 2 до 4
+//console.log(Math.random() * (4 - 2) + 2);
+//console.log(Math.round(Math.random()*(4 - 2) + 2));
+
+//***7**
+//дізнатись довжину message
+//const message = "Welcome to Bahamas!"; //19
+//console.log(message.length);
+
+//***8**
+//вивести в консоль message  великими літерами
+//console.log(message.toUpperCase()); // WELCOME TO BAHAMAS!
+
+//***9**
+// створити пустий об*єкт
+//    let obj = {};
+// додати туди ім*я, вік і місто
+//    obj.name = "Piter",
+//    obj.age = 50,
+ //   obj.city = "Trutnov",
+// вивести результат в консоль
+//console.log(obj);
+// видалити місто
+//   delete obj.city;
+// додати буль з ключем: like flowers
+//    obj["like flowers"] = true;
+// вивести результат в консоль
+//  console.log(obj);
+
+//***10**
+// За допомогою циклу  “for…in” вивести в консоль ключі і значення об*єкта
+
+/*for (let key in obj) {
+    console.log(key);
+    console.log(obj[key]);
+    }*/
+
